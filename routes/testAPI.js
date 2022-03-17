bf882476ed257b0992df6c4c6095f7dfc39cacd1
rchan3 <r.achiechan@gmail.com>
2022-03-17 15:43:38 -0400

    changed srcurl image location to assets directory

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -4,19 +4,21 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   res.send([
     {
-      srcURL: "./32fa76e072b091a28d3aa8577453cdfd_7473354622486228234.jpg",
+      srcURL:
+        "./assets/32fa76e072b091a28d3aa8577453cdfd_7473354622486228234.jpg",
       srcAlt: "",
     },
     {
-      srcURL: "./235af4e62159910421fbcffbf1cd9c2e_2611120445102707012.jpg",
+      srcURL:
+        "./assets/235af4e62159910421fbcffbf1cd9c2e_2611120445102707012.jpg",
       srcAlt: "",
     },
     {
-      srcURL: "./2022021610120592184.png",
+      srcURL: "./assets/2022021610120592184.png",
       srcAlt: "",
     },
     {
-      srcURL: "./2022021510344490430.jpg",
+      srcURL: "./assets/2022021510344490430.jpg",
       srcAlt: "",
     },
   ]);
